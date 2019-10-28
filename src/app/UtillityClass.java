043799e091f1ae2186f8e3eac5f84c56f84cd432
richierr <radomir.ribic@gmail.com>
2019-10-28 22:08:40 +0100

    push sa desktopa

--- src/app/UtillityClass.java
+++ src/app/UtillityClass.java
@@ -1,5 +1,42 @@
 package app;
 
-public class UtillityClass {
 
+import java.util.List;
+
+import javax.swing.SwingWorker;
+
+
+public class UtillityClass {
+	
+	public static void stopWatch() {
+		SwingWorker<Boolean, String> stopWatchThread=new SwingWorker<Boolean, String>(){
+			long startTime;
+			long endTime;
+
+			@Override
+			protected Boolean doInBackground() throws Exception {
+				startTime=System.currentTimeMillis();
+				
+				
+				
+				return null;
+			}
+
+			@Override
+			protected void process(List<String> chunks) {
+				// TODO Auto-generated method stub
+				super.process(chunks);
+			};
+			
+
+			
+		
+		
+		
+		
+		
+	};
+	
+
+}
 }
